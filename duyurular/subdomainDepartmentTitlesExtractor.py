ece148ef4cce69a01fa72ada9eabf45831539311
Yunus Emre Gül <yunemregul@gmail.com>
2021-02-20 08:06:49 +0300

    Duyuru sayfalarının başlıkları bulundu

--- duyurular/subdomainDepartmentTitlesExtractor.py
+++ duyurular/subdomainDepartmentTitlesExtractor.py
@@ -1,19 +1,27 @@
 import requests
 import json
+from lxml.html import fromstring
 
-validSubdomains = []
-
+validSubdomainTitles = {}
 with open("validSubdomains.txt") as f:
     data = json.load(f)
     for site in data:
         try:
-            request = requests.head("http://"+site+"/duyurular.php", timeout=2)
+            request = requests.get("http://"+site+"/duyurular.php", timeout=2)
         except Exception:
             print("-" + site)
             continue
 
         if request.status_code == 200:
-            validSubdomains.append(site)
-            print("+" + site)
+            tree = fromstring(request.content)
+            title = tree.findtext('.//title')
+            if title:
+                validSubdomainTitles["http://"+site+"/duyurular.php"] = title
+                print("+" + site + " = " + title)
+            else:
+                print("-" + site + " ! TITLE NOT FOUND IN PAGE !")
         else:
             print("-" + site)
+
+with open("departmentDuyurularUrlAndTitles.txt", "w") as f:
+    json.dump(validSubdomainTitles, f, indent=2, sort_keys=True, ensure_ascii=False)
